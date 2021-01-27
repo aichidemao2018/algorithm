26b4c4d16fd44b9c55d61cd0d21c78eb977b01c6
maoqiang <maoqiang@ticketdashi.com>
2021-01-28 00:00:10 +0800

    字符串反转 有序列表合并

---
+++ src/main/java/com/algorithm/leetcode/Array/MaxSubArray.java
@@ -0,0 +1,10 @@
+package com.algorithm.leetcode.Array;
+
+/**
+ * 53。最大子序列和
+ */
+public class MaxSubArray {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/algorithm/leetcode/Array/RemoveDuplicates.java
+++ src/main/java/com/algorithm/leetcode/Array/RemoveDuplicates.java
@@ -1,4 +1,29 @@
 package com.algorithm.leetcode.Array;
 
+/**
+ * 删除重复元素
+ * 快慢指针
+ */
 public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 2};
+        int solution = solution(nums);
+        System.out.println(solution);
+    }
+
+    private static int solution(int num[]) {
+
+        if (num.length == 0) {
+            return 0;
+        }
+        int i = 0;
+        for (int j = 1; j < num.length; j++) {
+            if (num[j] != num[i]) {
+                i++;
+                num[i] = num[j];
+            }
+
+        }
+        return i + 1;
+    }
 }
