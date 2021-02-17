9c8a252673d98a7ef212abed4d58a64dbbece3b9
maoqiang <maoqiang@ticketdashi.com>
2021-02-17 22:35:29 +0800

    层次遍历

---
+++ src/main/java/com/algorithm/leetcode/Array/Merge.java
@@ -0,0 +1,15 @@
+package com.algorithm.leetcode.Array;
+
+/**
+ * 区间合并
+ */
+public class Merge {
+    public static void main(String[] args) {
+
+    }
+
+    public int[][] merge(int[][] intervals) {
+
+        return null;
+    }
+}
---
+++ src/main/java/com/algorithm/leetcode/tree/LevelOrder.java
@@ -0,0 +1,44 @@
+package com.algorithm.leetcode.tree;
+
+import com.algorithm.leetcode.TreeNode;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层次遍历
+ */
+public class LevelOrder {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        Queue<TreeNode> queues = new ArrayDeque<TreeNode>();
+        if (root != null) {
+            queues.add(root);
+        }
+        while (!queues.isEmpty()) {
+            int length = queues.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < length; i++) {
+                TreeNode node = queues.poll();
+                level.add(node.getVal());
+                if (node.getLeft() != null) {
+                    queues.add(node.getLeft());
+                }
+                if (node.getRight() != null) {
+                    queues.add(node.getRight());
+                }
+
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+}
